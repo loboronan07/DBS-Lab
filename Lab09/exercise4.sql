121230581f953c14e052b0960eafcc4c857d45f5
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-01 23:40:58 +0530

    Update exercise4.sql

--- Lab09/exercise4.sql
+++ Lab09/exercise4.sql
@@ -1,15 +1,14 @@
-set serveroutput on;
-
-declare 
-    cursor c is select * from takes natural join student;
-
-begin 
-    for stud in c
-    loop
-        if stud.course_id='CS-101' and stud.tot_cred<30 then
-            delete from takes where id=stud.id;
-        end if;
-    end loop;
-
+declare
+	cursor ex4 is select * from takes;
+	cred student.tot_cred%type;
+begin
+	for val in ex4 loop
+		if val.course_id = 'CS-101' then
+			select tot_cred into cred from student where id = val.id;
+			if cred < 30 then 
+				delete from takes where id = val.id and course_id = 'CS-101' and semester = val.semester and year = val.year;
+			end if;
+		end if;
+	end loop;
 end;
 /
