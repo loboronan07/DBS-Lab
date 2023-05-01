177fb8e0db0c67749081e7ddd6d36b62f16d08d6
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-01 22:08:58 +0530

    Update createtable.sql

--- Lab09/createtable.sql
+++ Lab09/createtable.sql
@@ -1,5 +1,6 @@
-create table salary_raise(
-    instructor_id varchar2(5) primary key,
-    raise_date date,
-    raise_amt number(8,2)
+create table salary_raise (
+	id	varchar(5),
+	raise_date date,
+    raise_amt numeric(8, 2),
+	foreign key(id) REFERENCES instructor on delete cascade
 );
