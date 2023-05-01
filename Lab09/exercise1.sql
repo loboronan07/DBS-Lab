3ad580bbb2b2978d0cca249a5fafc219b21a04b5
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-01 22:08:27 +0530

    Update exercise1.sql

--- Lab09/exercise1.sql
+++ Lab09/exercise1.sql
@@ -1,18 +1,22 @@
 set serveroutput on;
 
 declare 
-    dname instructor.dept_name%type;
-    cursor c(dname instructor.dept_name%type) is select * from instructor where dept_name=dname;
+	dname instructor.dept_name%type;
+	ins instructor%rowtype;
+	cursor ex1 is select * from instructor;
+begin
+	dname := '&dept_name';
 
-begin 
-    dname := '&dept_name';
-
-    for ins in c(dname)
-    loop
-        insert into salary_raise values(ins.id, current_date, ins.salary*0.05);
-        update instructor set salary=salary*1.05 where id=ins.id;
-    end loop;
-
+	open ex1;
+	loop
+		fetch ex1 into ins;
+		exit when ex1%notfound;
+		if ins.dept_name = dname then
+			insert into salary_raise values (ins.id, current_date, ins.salary*0.05);
+			update instructor set salary = salary * 1.05 where id = ins.id;
+		end if;
+	end loop;
+	close ex1;
 end;
-/
+/	
 
