66906d68b9a8bdc898fdf462f08c9e0ce796d63d
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-02 01:22:15 +0530

    Update exercise2.sql

--- Lab10/exercise2.sql
+++ Lab10/exercise2.sql
@@ -1,27 +1,24 @@
-set serveroutput on;
+create or replace procedure disp_by_department(dname department.dept_name%type) as
+	cursor get_ins is select name from instructor where dept_name = dname;
+	cursor get_course is select title from course where dept_name = dname;
+begin
+    dbms_output.put_line('Under Department ' || dname);
+	dbms_output.put_line('---Instructors---');
+	for ins in get_ins loop
+		dbms_output.put_line(ins.name);
+	end loop;
 
-create or replace procedure listing(dpt_nm department.dept_name%type) is
-    cursor c1 is select * from instructor where dept_name=dpt_nm;
-    cursor c2 is select * from course where dept_name=dpt_nm;
-    begin
-        dbms_output.put_line('Instructors associated with Department ' || dpt_nm);
-        for ins in c1
-        loop 
-            dbms_output.put_line(ins.name);
-        end loop;
-
-        dbms_output.put_line('Courses associated with Department ' || dpt_nm);
-        for crs in c2
-        loop 
-            dbms_output.put_line(crs.title);
-        end loop;
-    end;
+	dbms_output.put_line('---Courses---');
+	for crs in get_course loop
+		dbms_output.put_line(crs.title);
+	end loop;
+end;
 /
 
 declare 
-    dpt_nm department.dept_name%type;
+    dname department.dept_name%type;
 begin
-    dpt_nm := '&department_name';
-    listing(dpt_nm);
+	dname := 'Comp. Sci.';
+	disp_by_department(dname);
 end;
 /
