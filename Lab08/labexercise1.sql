bb20be9f725b4061f6c9497edd7e7392bd2c63d2
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-02 00:19:20 +0530

    Update labexercise1.sql

--- Lab08/labexercise1.sql
+++ Lab08/labexercise1.sql
@@ -1,11 +1,10 @@
-set serveroutput on;
-declare
-    roll StudentTable.RollNo%type;
-    g StudentTable.GPA%type;
+declare 
+	r student_table.rollno%type;
+	g student_table.gpa%type;
 begin 
-    roll := &roll;
-    select GPA into g from StudentTable where RollNo=roll; 
-    dbms_output.put_line('GPA of Student with Roll No ' || to_char(roll) || ' is ' || to_char(g));
+	r := &roll;
+	select gpa into g from student_table where rollno = r;
+	dbms_output.put_line('GPA of Student with Roll No ' || r || ' is ' || g);
 exception
     when no_data_found then
         dbms_output.put_line('Student does not exist');
