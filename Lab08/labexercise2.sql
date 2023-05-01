789c3a3c6af99e49cc0ee63497494f7c9a1a77bd
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-02 00:19:47 +0530

    Update labexercise2.sql

--- Lab08/labexercise2.sql
+++ Lab08/labexercise2.sql
@@ -1,35 +1,36 @@
-set serveroutput on;
-
-declare
-    roll StudentTable.RollNo%type;
-    g StudentTable.GPA%type;
-    grade varchar(2);
-
+declare 
+	r student_table.rollno%type;
+	g student_table.gpa%type;
+	grade varchar(2);
 begin 
-    roll := &roll;
-    select GPA into g from StudentTable where RollNo=roll; 
-    
-    if g < 4 then
+	r := &roll;
+	select gpa into g from student_table where rollno = r;
+
+	if g < 0 then
+        dbms_output.put_line('GPA cannot be less than 0 for a student.');
+        return;
+	elsif g <= 4 then
         grade := 'F';
-    elsif g < 5 then 
+	elsif g <=5 then
         grade := 'E';
-    elsif g < 6 then 
+	elsif g <=6 then
         grade := 'D';
-    elsif g < 7 then 
+	elsif g <=7 then
         grade := 'C';
-    elsif g < 8 then 
+	elsif g <=8 then
         grade := 'B';
-    elsif g < 9 then 
+	elsif g <=9 then
         grade := 'A';
-    else 
+	elsif g <= 10 then
         grade := 'A+';
-    end if;
+	else 
+        dbms_output.put_line('GPA cannot be more than 10 for a student.');
+        return;
+	end if;
 
-    dbms_output.put_line('Grade of Student ' || to_char(roll) || ': ' || grade);
-
+	dbms_output.put_line('Grade of Student with Roll No ' || r || ' is ' || grade);
 exception
     when no_data_found then
         dbms_output.put_line('Student does not exist');
-
 end;
 /
