da1e0d141de7836e0d0e6a6e1a35b1c69cc8fed1
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-02 01:06:25 +0530

    Update exercise8.sql

--- Lab09/exercise8.sql
+++ Lab09/exercise8.sql
@@ -1,27 +1,24 @@
-set serveroutput on;
-
 declare 
-    bud number(12, 2);
-    sal number(12, 2);
-    cursor c is select * from instructor where dept_name='Biology';
+    sal_sum numeric(8, 2);
+    budg numeric(8, 2);
+	cursor ex8(dname instructor.dept_name%type) is select * from instructor where dept_name = dname for update;
+begin
+	savepoint save_before;
 
-begin 
-    commit;
+	for ins in ex8('Biology') loop
+		update instructor set salary = salary*1.2 where current of ex8;
+	end loop;
 
-    for ins in c
-    loop
-        update instructor set salary=salary*1.2 where id=ins.id;
-    end loop;
+	select sum(salary) into sal_sum from instructor where dept_name = 'Biology';
+	select budget into budg from department where dept_name = 'Biology';
 
-    select sum(salary) into sal from instructor where dept_name='Biology';
-    select budget into bud from department where dept_name='Biology';
-
-    if bud > sal then
-        dbms_output.put_line('Successfully updated salaries with raise of 20 percent.');
-    else
-        rollback;
+	if sal_sum > budg then 
         dbms_output.put_line('Department Budget not enough to support raise.');
-    end if;
+        rollback to savepoint save_before;
+	else 
+        dbms_output.put_line('Successfully updated salaries with raise of 20 percent.');
+	end if;
 
+	commit;
 end;
 /
