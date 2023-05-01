ca12ef7cecfb59b79c30e66e7e7b4aeeab512138
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-02 00:21:42 +0530

    Update labexercise9.sql

--- Lab08/labexercise9.sql
+++ Lab08/labexercise9.sql
@@ -1,48 +1,45 @@
-set serveroutput on;
+insert into student_table(rollno, gpa) values (6, -10);
+/
 
-declare
-    out_of_range exception;
-    roll StudentTable.RollNo%type;
-    g StudentTable.GPA%type;
-    grade varchar(2);
-
+declare 
+    out_of_bounds exception;
+	r student_table.rollno%type;
+	g student_table.gpa%type;
+	grade varchar2(2);
 begin 
-    roll := 1;
-    while roll < 9
-        loop
-            select GPA into g from StudentTable where RollNo=roll; 
+	r := 1;
 
-            if g < 0 then 
-                raise out_of_range;
-            elsif g < 4 then
-                grade := 'F';
-            elsif g < 5 then 
-                grade := 'E';
-            elsif g < 6 then 
-                grade := 'D';
-            elsif g < 7 then 
-                grade := 'C';
-            elsif g < 8 then 
-                grade := 'B';
-            elsif g < 9 then 
-                grade := 'A';
-            elsif g <= 10 then 
-                grade := 'A+';
-            else
-                raise out_of_range;
-            end if;
+	while r <= 6 loop
+		select gpa into g from student_table where rollno = r;
 
-            update StudentTable set LetterGrade=grade where RollNo=roll;
+		if g < 0 then
+        	raise out_of_bounds;
+		elsif g <= 4 then
+        	grade := 'F';
+		elsif g <=5 then
+        	grade := 'E';
+		elsif g <=6 then
+        	grade := 'D';
+		elsif g <=7 then
+        	grade := 'C';
+		elsif g <=8 then
+        	grade := 'B';
+		elsif g <=9 then
+        	grade := 'A';
+		elsif g <= 10 then
+        	grade := 'A+';
+		else 
+        	raise out_of_bounds;
+		end if;
 
-            roll := roll + 1;
-        end loop;
+		update student_table set lettergrade = grade where rollno = r;
 
+		r := r + 1;
+	end loop;
 exception
     when no_data_found then
         dbms_output.put_line('Student does not exist');
-
-    when out_of_range then 
-        dbms_output.put_line('GPA out of range');
-
+    when out_of_bounds then
+        dbms_output.put_line('GPA value out of range 0-10.');
 end;
 /
